27fff7b35c51d2098def2993dc1167bd448c6ab7
Luan Albuquerque dos Santos <51758832+luan-albuquerque@users.noreply.github.com>
2022-03-05 20:41:12 -0400

    Deletado metodologia

--- src/modules/collaborators/useCases/CreateCollaborator/CreateCollaboratorController.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-
-class CreateCollaboratorController {
-  handle(request: Request, response: Response): Response {
-    return response.status(201).json("success");
-  }
-}
-export { CreateCollaboratorController };
