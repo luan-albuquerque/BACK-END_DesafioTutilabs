782c4a12034fd5d847c545d55664eb76d88f4a2e
Luan Albuquerque dos Santos <51758832+luan-albuquerque@users.noreply.github.com>
2022-03-07 02:38:55 -0400

    Verifcação de token

--- src/routes/authenticate.routes.ts
+++ src/routes/authenticate.routes.ts
@@ -1,10 +1,14 @@
 import { authenticateUserController } from "@modules/collaborators/useCases/AuthenticateUser";
 import { Router } from "express";
+import { ensureAuthenticated } from "infra/middlewares/ensureAuthenticated";
+import { verifyAuthenticated } from "infra/middlewares/verifyAuthenticated";
 
 const authenticateRoutes = Router();
 
 authenticateRoutes.post("/", (request, response) => {
   return authenticateUserController.handle(request, response);
 });
+
+authenticateRoutes.get("/verify", verifyAuthenticated);
 
 export { authenticateRoutes };
