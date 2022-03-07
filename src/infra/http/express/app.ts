9096d46ddd75133e9a7e859d1a0351e37157da6c
Luan Albuquerque dos Santos <51758832+luan-albuquerque@users.noreply.github.com>
2022-03-06 20:49:27 -0400

    Adicionando CORD para consumo de API

--- src/infra/http/express/app.ts
+++ src/infra/http/express/app.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 
@@ -9,6 +10,7 @@ import { routes } from "../../../routes";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
